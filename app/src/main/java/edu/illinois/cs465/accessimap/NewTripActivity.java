0603ec2aeb68d7882478edd6fe658cc10b948b80
anikajain03 <46655702+anikajain03@users.noreply.github.com>
2023-11-12 21:21:29 -0600

    New trip page with autocomplete text entries (functionality still needs work based on back end coordination)

--- app/src/main/java/edu/illinois/cs465/accessimap/NewTripActivity.java
+++ app/src/main/java/edu/illinois/cs465/accessimap/NewTripActivity.java
@@ -2,7 +2,9 @@ package edu.illinois.cs465.accessimap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewTripActivity extends AppCompatActivity
 {
@@ -12,5 +14,16 @@ public class NewTripActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip);
+    }
+
+    public void accessHomeScreen(View view) {
+        Intent intent = new Intent(NewTripActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    // TODO - fix button linking to according nav page
+    public void openIndoorNav(View view) {
+        Intent intent = new Intent(NewTripActivity.this, IndoorNavActivity.class);
+        startActivity(intent);
     }
 }
