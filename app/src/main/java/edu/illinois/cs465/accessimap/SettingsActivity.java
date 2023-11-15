1c91e53f8847e24a050a4c38d5e5f44d42ba2863
anikajain03 <46655702+anikajain03@users.noreply.github.com>
2023-11-15 13:04:34 -0600

    Save settings state when accessing page again

--- app/src/main/java/edu/illinois/cs465/accessimap/SettingsActivity.java
+++ app/src/main/java/edu/illinois/cs465/accessimap/SettingsActivity.java
@@ -1,6 +1,7 @@
 package edu.illinois.cs465.accessimap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
 
 import android.content.Intent;
 
@@ -11,10 +12,16 @@ import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity
 {
-    private Switch rampSwitch;
-    private Switch elevatorSwitch;
+    private Switch rampSwitch, elevatorSwitch, bathroomSwitch;
 
     private Intent homeIntent;
+    private SharedPreferences sp;
+
+    private static final String PREF_NAME = "AccessPrefs";
+    private static final String RAMP_SWITCH_STATE = "rampSwitchState";
+    private static final String ELEVATOR_SWITCH_STATE = "elevatorSwitchState";
+    private static final String BATHROOM_SWITCH_STATE = "bathroomSwitchState";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,34 +31,67 @@ public class SettingsActivity extends AppCompatActivity
 
         rampSwitch = findViewById(R.id.ramps);
         elevatorSwitch = findViewById(R.id.elevators);
+        bathroomSwitch = findViewById(R.id.bathrooms);
 
-        // Set a listener for switch1
+        sp = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+
+        // Retrieve the last saved state of the switches
+        boolean savedRampSwitchState = sp.getBoolean(RAMP_SWITCH_STATE, false);
+        boolean savedElevatorSwitchState = sp.getBoolean(ELEVATOR_SWITCH_STATE, false);
+        boolean savedBathroomSwitchState = sp.getBoolean(BATHROOM_SWITCH_STATE, false);
+
+        // Set the switches to their last saved state
+        rampSwitch.setChecked(savedRampSwitchState);
+        elevatorSwitch.setChecked(savedElevatorSwitchState);
+        bathroomSwitch.setChecked(savedBathroomSwitchState);
+
+        // Set a listener for rampSwitch
         rampSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 homeIntent = null;
+                saveSwitchState(RAMP_SWITCH_STATE, isChecked);
             }
         });
 
-        // Set a listener for switch2
+        // Set a listener for elevatorSwitch
         elevatorSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 homeIntent = null;
+                saveSwitchState(ELEVATOR_SWITCH_STATE, isChecked);
             }
         });
+
+        // Set a listener for bathroomSwitch
+        bathroomSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                homeIntent = null;
+                saveSwitchState(BATHROOM_SWITCH_STATE, isChecked);
+            }
+        });
+    }
+
+    private void saveSwitchState(String switchKey, boolean isChecked)
+    {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(switchKey, isChecked);
+        editor.apply();
     }
 
     public void updateText() {
         boolean rampSwitchState = rampSwitch.isChecked();
         boolean elevatorSwitchState = elevatorSwitch.isChecked();
+        boolean bathroomSwitchState = bathroomSwitch.isChecked();
 
         if (homeIntent == null) {
             homeIntent = new Intent(SettingsActivity.this, MainActivity.class);
         }
 
-        homeIntent.putExtra("RAMP_SWITCH_STATE", rampSwitchState);
-        homeIntent.putExtra("ELEVATOR_SWITCH_STATE", elevatorSwitchState);
+        homeIntent.putExtra(RAMP_SWITCH_STATE, rampSwitchState);
+        homeIntent.putExtra(ELEVATOR_SWITCH_STATE, elevatorSwitchState);
+        homeIntent.putExtra(BATHROOM_SWITCH_STATE, bathroomSwitchState);
     }
 
     public void accessHomeScreen(View view) {
