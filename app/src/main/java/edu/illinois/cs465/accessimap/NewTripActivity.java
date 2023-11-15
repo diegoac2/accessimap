41f7ffbb0268e489f0df8af70f45fba202a5886b
AJYChen <andrew83@illinois.edu>
2023-11-15 11:03:11 -0600

    Push New Version with Dropdown Enable/Disable

--- app/src/main/java/edu/illinois/cs465/accessimap/NewTripActivity.java
+++ app/src/main/java/edu/illinois/cs465/accessimap/NewTripActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class NewTripActivity extends AppCompatActivity
 {
+    private Spinner buildingSpinnerFrom;
+    private Spinner roomSpinnerFrom;
+    private Spinner buildingSpinnerTo;
+    private Spinner roomSpinnerTo;
+
+    // Map to store room options based on the selected building
+    private Map<String, List<String>> buildingRoomMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,33 +48,153 @@ public class NewTripActivity extends AppCompatActivity
             textView.setVisibility(View.GONE);
             radioGroup.setVisibility(View.GONE);
         }
-        Spinner buildingSpinnerFrom = findViewById(R.id.building_spinner_from);
-        Spinner roomSpinnerFrom = findViewById(R.id.room_spinner_from);
-        Spinner buildingSpinnerTo = findViewById(R.id.building_spinner_to);
-        Spinner roomSpinnerTo = findViewById(R.id.room_spinner_to);
+        buildingSpinnerFrom = findViewById(R.id.building_spinner_from);
+        roomSpinnerFrom = findViewById(R.id.room_spinner_from);
+        buildingSpinnerTo = findViewById(R.id.building_spinner_to);
+        roomSpinnerTo = findViewById(R.id.room_spinner_to);
+
+        // Initialize the buildingRoomMap
+        buildingRoomMap = new HashMap<>();
+        buildingRoomMap.put("Building X", generateRoomOptionsForBuildingX());
+        buildingRoomMap.put("Building Y", generateRoomOptionsForBuildingY());
+
+        // Set up initial spinner options
+        setUpBuildingSpinner(buildingSpinnerFrom);
+        setUpBuildingSpinner(buildingSpinnerTo);
+
+        setUpRoomSpinner(roomSpinnerFrom, buildingSpinnerFrom);
+        setUpRoomSpinner(roomSpinnerTo, buildingSpinnerTo);
+
 
         // Assuming you have an array or list of buildings and rooms
-        String[] buildings = {"Building X", "Building Y"};
-        String[] rooms = {"Room A", "Room B", "Room C"};
+//        String[] buildings = {"Building X", "Building Y"};
+//        String[] rooms = {"Room A", "Room B", "Room C"};
 
         // Create ArrayAdapter for buildings
-        ArrayAdapter<String> buildingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, buildings);
+//        ArrayAdapter<String> buildingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, buildings);
+//        buildingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter for building Spinners
+//        buildingSpinnerFrom.setAdapter(buildingAdapter);
+//        buildingSpinnerTo.setAdapter(buildingAdapter);
+
+        // Create ArrayAdapter for rooms
+//        ArrayAdapter<String> roomAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, rooms);
+//        roomAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter for room Spinners
+//        roomSpinnerFrom.setAdapter(roomAdapter);
+//        roomSpinnerTo.setAdapter(roomAdapter);
+
+    }
+    private void setUpBuildingSpinner(Spinner spinner) {
+        // Create ArrayAdapter for buildings
+        ArrayAdapter<String> buildingAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                generateBuildingOptions());
         buildingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Set the ArrayAdapter for building Spinners
-        buildingSpinnerFrom.setAdapter(buildingAdapter);
-        buildingSpinnerTo.setAdapter(buildingAdapter);
+        spinner.setAdapter(buildingAdapter);
 
-        // Create ArrayAdapter for rooms
-        ArrayAdapter<String> roomAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, rooms);
-        roomAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Set initial selection to "Select a building"
+        spinner.setSelection(buildingAdapter.getPosition("Select a building"));
 
-        // Set the ArrayAdapter for room Spinners
-        roomSpinnerFrom.setAdapter(roomAdapter);
-        roomSpinnerTo.setAdapter(roomAdapter);
+        // Set item selected listener for building Spinners
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedBuilding = (String) parentView.getItemAtPosition(position);
 
+                // Update room options based on the selected building
+                updateRoomOptions(selectedBuilding, spinner);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here
+            }
+        });
     }
 
+    private void setUpRoomSpinner(Spinner roomSpinner, Spinner buildingSpinner) {
+        // Set up room Spinner with an empty list initially
+        ArrayAdapter<String> roomAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new ArrayList<>());
+        roomAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        roomSpinner.setAdapter(roomAdapter);
+        roomSpinner.setEnabled(false);
+
+        // Set item selected listener for room Spinner
+        roomSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Handle room selection if needed
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here
+            }
+        });
+
+        // Set item selected listener for building Spinner
+        buildingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedBuilding = (String) parentView.getItemAtPosition(position);
+
+                // Update room options based on the selected building
+                updateRoomOptions(selectedBuilding, roomSpinner);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here
+            }
+        });
+    }
+
+    private void updateRoomOptions(String selectedBuilding, Spinner roomSpinner) {
+        // Update room options based on the selected building
+        List<String> roomOptions = buildingRoomMap.get(selectedBuilding);
+        ArrayAdapter<String> roomAdapter = (ArrayAdapter<String>) roomSpinner.getAdapter();
+        if (roomOptions != null) {
+            roomAdapter.clear();
+            roomAdapter.addAll(roomOptions);
+            roomAdapter.notifyDataSetChanged();
+            roomSpinner.setEnabled(true);
+        } else {
+            // Handle the case where roomOptions is null (optional)
+            // You might want to set roomSpinner to a default state or log an error.
+            roomAdapter.clear();
+            roomAdapter.notifyDataSetChanged();
+            roomSpinner.setEnabled(false);
+        }
+    }
+    private List<String> generateBuildingOptions() {
+        List<String> buildings = new ArrayList<>();
+        buildings.add("Select a building");
+        buildings.add("Building X");
+        buildings.add("Building Y");
+        return buildings;
+    }
+
+    private List<String> generateRoomOptionsForBuildingX() {
+        // TODO: Add your logic to generate room options for Building X
+        List<String> rooms = new ArrayList<>();
+        rooms.add("Room X1");
+        rooms.add("Room X2");
+        rooms.add("Room X3");
+        return rooms;
+    }
+
+    private List<String> generateRoomOptionsForBuildingY() {
+        // TODO: Add your logic to generate room options for Building Y
+        List<String> rooms = new ArrayList<>();
+        rooms.add("Room Y1");
+        rooms.add("Room Y2");
+        rooms.add("Room Y3");
+        return rooms;
+    }
     public void accessHomeScreen(View view) {
         finish();
     }
